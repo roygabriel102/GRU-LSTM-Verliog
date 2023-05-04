7fd12220515494453a8c3cb4aefc340974a8df9c
abdullahsahruri <71990122+abdullahsahruri@users.noreply.github.com>
2023-05-03 23:18:07 -0500

    Update ConcatMultAdd.v
    Define out1 as 8 bits rather than 1 bit

--- ConcatMultAdd.v
+++ ConcatMultAdd.v
@@ -1,8 +1,8 @@
-module ConcatMultAdd(X, h_in, W0, W1, b, out);	
+module ConcatMultAdd#(DATA_WIDTH = 8, FRACT_WIDTH = 5)(X, h_in, W0, W1, b, out);	
 	// Concatenates and Adds
 	// incoming data signed and fixed width
-	parameter DATA_WIDTH = 16;
-	parameter FRACT_WIDTH = 8;
+//	parameter DATA_WIDTH = 16;
+//	parameter FRACT_WIDTH = 8;
 	
 	input signed [DATA_WIDTH-1:0] X, h_in;
 	input signed [DATA_WIDTH-1:0] W0, W1, b;
@@ -12,7 +12,7 @@ module ConcatMultAdd(X, h_in, W0, W1, b, out);
 	wire signed [DATA_WIDTH-1:0] p1,p2;
 	wire signed [DATA_WIDTH-1:0] temp;
 	wire cout, cout2;
-	
+	wire signed [DATA_WIDTH-1:0] out1; //Added this for data widthh correction
 	// behavior: out = W*{x,h_in} + b where W={W0,W1}
 	qmult #(DATA_WIDTH, FRACT_WIDTH) m1 (X,W0, p1, );
 	qmult #(DATA_WIDTH, FRACT_WIDTH) m2 (h_in, W1, p2, );
