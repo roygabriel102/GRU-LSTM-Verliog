9e22003e661142a402627e1612a522168f8951ab
abdullahsahruri <71990122+abdullahsahruri@users.noreply.github.com>
2023-05-03 23:16:45 -0500

    Update Test_v2.v
    Add bias and weights as inputs

--- Test_v2.v
+++ Test_v2.v
@@ -4,8 +4,13 @@ module gru_lstm_tb;
 
 	// Inputs
 //	reg [15:0] c_in;
-	reg [7:0] h_in;
-	reg [7:0] X;
+	// All inputs are reconfigurable
+    	parameter DATA_WIDTH = 8;
+	parameter FRACT_WIDTH = 5;
+	reg [DATA_WIDTH-1:0] h_in;
+	reg [DATA_WIDTH-1:0] X;
+	reg [DATA_WIDTH-1:0] Wz, Wr, Wh, Uz, Ur, Uh;
+	reg [DATA_WIDTH-1:0] bz, br, bh;
 
 	// Outputs
 //	wire [15:0] c_out;
@@ -15,7 +20,8 @@ module gru_lstm_tb;
 	gru_lstm_cell uut (
 		.h_in(h_in), 
 		.X(X), 
-		.h_out(h_out)
+		.h_out(h_out),
+		.Wz(Wz), .Wr(Wr), .Wh(Wh), .Uz(Uz), .Ur(Ur), .Uh(Uh), .bz(bz), .br(br), .bh(bh) //the weight bias were declared as inputs in main.
 	);
 
 	initial begin
@@ -30,34 +36,45 @@ module gru_lstm_tb;
 		// First 8 bits are integeral part and last 8 bits are fractional
 		// All Numbers are signed 
 		
-        #200;
+        #10;
 		X=-8'h80;       
 //		c_in=-16'h0100;    // c_in = -1
 		h_in=8'h80;     
 
 		$monitor("Output (After Activation):%d ",h_out);
 		
-		#200;
+		#10;
      		X=8'h10;
 //		c_in=16'h0110;
 		h_in=8'h11;
 
 		$monitor("Output (After Activation):%d ",h_out);
 		
-		#200;
+		#10;
 	 	X=-8'h10;
 //		c_in=16'h0120;
 		h_in=8'h11;
 
 		$monitor("Output (After Activation):%d ",h_out);
-		
-		#200;
-      		X=8'h01;
+		forever
+		begin
+		#10;
+		//Randomized values for weights and bias
+      		 X= $random;
+      		Wz= $random;
+      		Wr= $random;
+      		Wh= $random;
+      		Uz= $random;
+      		Ur= $random; 
+      		Uh= $random;
+      		bz= $random;
+      		br= $random;
+      		bh= $random;
 //		c_in=16'h0210;
-		h_in=-8'h00;
+		h_in=$random;
 
 		$monitor("Output (After Activation):%d ",h_out);
-
+        end
 	end
       
 endmodule
