27bddd096d407713296129ad17afbe2c61641a3a
abdullahsahruri <71990122+abdullahsahruri@users.noreply.github.com>
2023-05-07 03:31:12 -0500

    Add file handling for data collection  Test_v2.v
    Added file handling for data collection.

--- Test_v2.v
+++ Test_v2.v
@@ -1,80 +1,71 @@
 `timescale 1ns / 1ps
 
 module gru_lstm_tb;
-
+//file_out.txt generated in C:\Users\*\Documents\GRU_LSTM\GRU_LSTM.sim\sim_1\behav\xsim
+//Added fopen, fwriteb, and fclose for file handling
 	// Inputs
-//	reg [15:0] c_in;
-	// All inputs are reconfigurable
-    	parameter DATA_WIDTH = 8;
+   	parameter DATA_WIDTH = 8;
 	parameter FRACT_WIDTH = 5;
+	integer file_out;
 	reg [DATA_WIDTH-1:0] h_in;
 	reg [DATA_WIDTH-1:0] X;
 	reg [DATA_WIDTH-1:0] Wz, Wr, Wh, Uz, Ur, Uh;
 	reg [DATA_WIDTH-1:0] bz, br, bh;
 
+
 	// Outputs
-//	wire [15:0] c_out;
 	wire [7:0] h_out;
 
 	// Instantiate the Unit Under Test (UUT)
-	gru_lstm_cell uut (
-		.h_in(h_in), 
-		.X(X), 
-		.h_out(h_out),
-		.Wz(Wz), .Wr(Wr), .Wh(Wh), .Uz(Uz), .Ur(Ur), .Uh(Uh), .bz(bz), .br(br), .bh(bh) //the weight bias were declared as inputs in main.
-	);
+gru_lstm_cell uut (
+.h_in(h_in), 
+.X(X), 
+.h_out(h_out),
+.Wz(Wz), .Wr(Wr), .Wh(Wh), .Uz(Uz), .Ur(Ur), .Uh(Uh), .bz(bz), .br(br), .bh(bh)
+);
 
-	initial begin
-		// Initialize Inputs
-//		c_in = 0;
-		h_in = 0;
-		X = 0;
+initial begin
+// Initialize Inputs
+h_in = 0;
+X = 0;
+Wz= $random;
+Wr= $random;
+Wh= $random;
+Uz= $random;
+Ur= $random; 
+Uh= $random;
+bz= $random;
+br= $random;
+bh= $random;
+file_out = $fopen("file_out.txt", "w");
 
-		// Wait 100 ns for global reset to finish
-		// Take not too large inputs such that they don't overflow and give proper results after right shift
-		// Preferably, take inputs <4 && >-4
-		// First 8 bits are integeral part and last 8 bits are fractional
-		// All Numbers are signed 
-		
-        #10;
-		X=-8'h80;       
-//		c_in=-16'h0100;    // c_in = -1
-		h_in=8'h80;     
 
-		$monitor("Output (After Activation):%d ",h_out);
-		
-		#10;
-     		X=8'h10;
-//		c_in=16'h0110;
-		h_in=8'h11;
+forever
+begin
+#10;
+ X= $random;
+Wz= $random;
+Wr= $random;
+Wh= $random;
+Uz= $random;
+Ur= $random; 
+Uh= $random;
+bz= $random;
+br= $random;
+bh= $random;
+h_in=$random;
 
-		$monitor("Output (After Activation):%d ",h_out);
-		
-		#10;
-	 	X=-8'h10;
-//		c_in=16'h0120;
-		h_in=8'h11;
 
-		$monitor("Output (After Activation):%d ",h_out);
-		forever
-		begin
-		#10;
-		//Randomized values for weights and bias
-      		 X= $random;
-      		Wz= $random;
-      		Wr= $random;
-      		Wh= $random;
-      		Uz= $random;
-      		Ur= $random; 
-      		Uh= $random;
-      		bz= $random;
-      		br= $random;
-      		bh= $random;
-//		c_in=16'h0210;
-		h_in=$random;
+$monitor("Output (After Activation):%d ",h_out);
+$fwriteb(file_out,h_out,"\n");
 
-		$monitor("Output (After Activation):%d ",h_out);
-        end
-	end
-      
+end
+
+end
+
+initial begin
+#300 // Adjust timestop
+$fclose(file_out);
+$finish;
+end
 endmodule
